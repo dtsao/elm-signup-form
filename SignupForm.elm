850771c8e2f3d285fc59e15eece902b01d778580
David Tsao <dtsao@users.noreply.github.com>
2015-10-18 19:58:28 -0400

    Added input validation and error messages

--- SignupForm.elm
+++ SignupForm.elm
@@ -1,19 +1,87 @@
 module SignupForm where
 
 
+import Effects
+import StartApp
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (id, type', for, value, class)
 
-view model =
+--MODEL
+initialErrors =
+    { username = "", password = ""}
+
+
+initialModel = 
+    { username = "", password = "", errors = initialErrors}
+
+
+--VIEW
+view actionDispatcher model =
     form [ id "signup-form" ] [
         h1 [] [ text "Sensational Signup Form" ]
         , label [ for "username-field" ] [ text "Username:" ]
-        , input [ id "username-field", type' "text", value model.username ] []
+        , input 
+            [ id "username-field"
+            , type' "text"
+            , value model.username
+            , on "input" targetValue (\str -> Signal.message actionDispatcher 
+                {actionType = "SET_USERNAME", payload = str}) 
+            ] 
+            []
+        , div [ class "validation-error" ] [ text model.errors.username ]
         , label [ for "password" ] [ text "password:" ]
-        , input [ id "password-field", type' "password", value model.password ] []
-        , div [ class "signup-button"] [ text "Sign Up!" ]
+        , input 
+            [ id "password-field"
+            , type' "password"
+            , value model.password 
+            , on "input" targetValue (\str -> Signal.message actionDispatcher
+                {actionType = "SET_PASSWORD", payload = str })
+            ] 
+            []
+        , div [ class "validation-error" ] [ text model.errors.password ]
+        , div [ class "signup-button", onClick actionDispatcher 
+                {actionType = "VALIDATE", payload = "" } ] [ text "Sign Up!" ]
     ]
 
+
+--UPDATE
+getErrors model =
+    { username = 
+        if model.username == "" then
+            "Please enter a username!"
+        else
+            ""
+
+    , password = 
+        if model.password == "" then
+            "Please enter a password!"
+        else
+            ""
+    }   
+
+
+update action model =
+    if 
+        | action.actionType == "VALIDATE" ->
+            ( { model | errors <- getErrors model }, Effects.none )
+        | action.actionType == "SET_USERNAME" ->
+            ( { model | username <- action.payload }, Effects.none )
+        | action.actionType == "SET_PASSWORD" ->
+            ( { model | password <- action.payload }, Effects.none )
+        | otherwise ->
+            ( model, Effects.none )
+
+
+--START
+app = 
+    StartApp.start 
+        { init = ( initialModel, Effects.none)
+        , update = update
+        , view = view
+        , inputs = []
+        }
+
+
 main =
-    view { username = "", password = ""}    
+    app.html
